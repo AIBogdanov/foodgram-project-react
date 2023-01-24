1c4db908ad3928428bae6e9a5457726912fb6eb4
Андрей Богданов <ower94@yandex.ru>
2023-01-24 23:09:27 +0300

    Admin_site-1.12

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -30,7 +30,7 @@ class IngredientAdmin(ModelAdmin):
 @register(Recipe)
 class RecipeAdmin(ModelAdmin):
     list_display = (
-        'name', 'author', 'get_image', 'tags'
+        'name', 'author', 'get_image', 'tags__name'
     )
     fields = (
         ('name', 'cooking_time',),
@@ -40,10 +40,10 @@ class RecipeAdmin(ModelAdmin):
     )
     raw_id_fields = ('author', )
     search_fields = (
-        'name', 'author', 'tags'
+        'name', 'author',
     )
     list_filter = (
-        'name', 'author__username',
+        'name', 'author__username', 'tags__name'
     )
 
     inlines = (IngredientInline,)
