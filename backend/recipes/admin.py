94d07c0000ad3cb920ffcff5e891d684ddd0e72b
Андрей Богданов <ower94@yandex.ru>
2023-01-25 01:03:39 +0300

    Admin_site-1.17

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -63,7 +63,7 @@ class RecipeAdmin(ModelAdmin):
     #     return ("\n".join([t.name for t in obj.tags.all()])).count()
     
     def get_is_favorited(self, obj):
-        return Recipe.favorites.through.objects.filter(id=obj.id).count()
+        return User.favorites.through.objects.filter(id=obj.id).count()
 
 @register(Tag)
 class TagAdmin(ModelAdmin):
