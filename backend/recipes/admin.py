ef43d2fc2826857fccd6a37d3d0439580114888d
Андрей Богданов <ower94@yandex.ru>
2023-01-26 10:55:46 +0300

    Админка 17

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -35,7 +35,7 @@ class IngredientAdmin(ModelAdmin):
 @register(Recipe)
 class RecipeAdmin(ModelAdmin):
     list_display = (
-        'name', 'author', 'get_image', 'tags.name',
+        'name', 'author', 'get_image', 'tag.name',
         'get_tags',
         'get_is_favorited',
     )
