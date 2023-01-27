2f725022325e3488aec661113a1616c226a74d5f
Андрей Богданов <ower94@yandex.ru>
2023-01-27 16:20:44 +0300

    Counter favorite 6

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,5 +1,5 @@
 from django.contrib.admin import (
-    ModelAdmin, StackedInline, register, site
+    ModelAdmin, StackedInline, register, site, TabularInline
 )
 from django.contrib.auth import get_user_model
 from django.utils.safestring import mark_safe
@@ -16,6 +16,17 @@ class IngredientInline(StackedInline):
     model = AmountIngredient
     extra = 2
     min_num = 1
+
+
+class MyInline(TabularInline):
+    model = Recipe
+
+    def get_extra(self, obj):
+        count = 0
+        for user in User.objects.all():
+            if user.favorites.filter(id=obj.id):
+                count += 1
+        return count
 
 
 @register(Ingredient)
@@ -45,9 +56,8 @@ class RecipeAdmin(ModelAdmin):
     list_filter = (
         'name', 'author__username', 'tags'
     )
-    inlines = (IngredientInline,)
+    inlines = (IngredientInline, MyInline)
     empty_value_display = EMPTY_VALUE_DISPLAY
-    actions_selection_counter = True
 
     def get_image(self, obj):
         return mark_safe(f'<img src={obj.image.url} width="80" hieght="30"')
@@ -68,7 +78,7 @@ class RecipeAdmin(ModelAdmin):
     get_tags.short_description = 'Теги'
     get_image.short_description = 'Изображение'
     get_is_favorited.short_description = 'Число добавлений в избранное'
-    
+
 
 
 @register(Tag)
